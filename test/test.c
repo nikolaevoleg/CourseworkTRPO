09692b71fe58d43bc1ce1b2cd4beb062beb360e2
creator228151 <mr.krivenkov1973@mail.ru>
2018-06-20 00:12:17 +0700

    add test

--- test/test.c
+++ test/test.c
@@ -1,8 +1,8 @@
 #define CTEST_MAIN
 
 #include <stdbool.h>
-#include "../thirdparty/ctest.h"
 #include <string.h>
+#include "../thirdparty/ctest.h"
 #include "../source/functions.h"
 #include "../source/game.h"
 
@@ -11,6 +11,13 @@ CTEST(INPUT_VALIDATION, FINE_INPUT)
 	char ch = 'a';
 	bool result = dotcheck(ch);
 	ASSERT_FALSE(result);
+}
+
+CTEST(NUMPOINTS_COINCIDENCE, T_POINTS)
+{
+	int result = checkStringsNum(2);
+	int expected = 0;
+	ASSERT_EQUAL(expected, result);
 }
 
 int main(int argc, const char** argv) 
