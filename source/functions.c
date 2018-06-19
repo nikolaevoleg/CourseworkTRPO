b0cddac7a0b9ff9cb27780112b319c74b4c6f39d
creator228151 <mr.krivenkov1973@mail.ru>
2018-06-19 19:29:43 +0700

    changing function.c

--- source/functions.c
+++ source/functions.c
@@ -1,12 +1,24 @@
+#include <stdlib.h>
 #include <stdio.h>
 #include <locale.h>
+#include <stdbool.h>
+
+bool dotcheck(char ch)
+{
+    if (ch == '.')
+        return true;
+    else
+        return false;
+}
 
 int mainMenu() {
-	
+
 	setlocale(LC_ALL, "Rus");
-	
+
+	system("cls");
+
 	int opt;
-	printf("\n**************************************\n1. Новая игра\n2. Текущий прогресс\n3. Выход\n\nВыберите действие: ");
+	printf("1. Новая игра\n2. Текущий прогресс\n3. Выход\n\nВыберите действие: ");
 	scanf("%d", &opt);
 
 	return opt;
@@ -17,10 +29,80 @@ int newGameMenu() {
 
 	setlocale(LC_ALL, "Rus");
 
+	system("cls");
+
 	int opt;
-	printf("\n**************************************\n1. Изучение новых слов\n2. Повторенее изученного\n3. Выход в главное меню\n\nВыберите действие: ");
+	printf("1. Изучение новых слов\n2. Повторенее изученного\n3. Выход в главное меню\n\nВыберите действие: ");
 	scanf("%d", &opt);
 
 	return opt;
+
+}
+
+int checkStringsNum(int fl) {
+
+	FILE *f;
+	if (fl == 1) {
+		f = fopen("../source/vocabulary.txt", "r");
+	}
+	else {
+		f = fopen("../source/studied.txt", "r");
+	}
+
+	char ch;
+	int num = 0;
+
+	while (!feof(f)) {
+		ch = getc(f);
+
+		if (dotcheck(ch) == true) {
+			num++;
+		}
+	}
+
+	fclose(f);
+
+	return num;
+
+}
+
+void wordsMass(int fl, char **eng, char **rus, int num) {
+
+	FILE *f;
+	if (fl == 1) {
+		f = fopen("../source/vocabulary.txt", "r");
+	}
+	else {
+		f = fopen("../source/studied.txt", "r");
+	}
+
+	char ch;
+	int symb;
+
+	for (int i = 0; i < num; i++) {
+
+		symb = 0;
+		ch = getc(f);
+		while (ch != '.' && (!feof(f))) {
+			eng[i][symb] = ch;
+			symb++;
+			ch = getc(f);
+		}
+		eng[i][symb] = '\0';
+
+		symb = 0;
+		ch = getc(f);
+		while ((ch != '\n') && (!feof(f))) {
+			rus[i][symb] = ch;
+			symb++;
+			ch = getc(f);
+		}
+		rus[i][symb] = '\0';
+
+	}
+
+	fclose(f);
+
+	return;
 
 }
